d05ede1a064a23a042870316c8c8eb07be69e8aa
Roman <roman.berti09@gmail.com>
2023-03-01 11:29:02 +0100

    Main.py funktion mit einem Schritt von LU
    Es muss noch eine funktion geschrieben werden, um diese funktion rekursive aufzurufen, wobei die Matrix jedes mal um 1 verkleiner wird

--- main.py
+++ main.py
@@ -4,13 +4,26 @@
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+"""---------Es gibt noch einen fehler bei aufgabe 3 aus ich versuche einen egenen Algorythmuss aus"""
+import numpy as np
+LR = np.array(np.random.rand(3,3))
+B = np.array(np.random.rand(3))
+print("LR:",LR,"\n")
 
+def forwardEl(A,b):
+    a = A
+    n = len(b)
+    l= A#np.zeros((n,n))
+    ln = np.empty([n,n])
+    m = 0.0
+    xs= 0
+    m = l[:,xs]/l[0,0]
+    m = np.delete(m,0)
+    for k in range(1,n):
+        l[k,:] -= m[k-1]*l[0,:]
+    print(l)
+    return l
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+forwardEl(LR,B)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
