a67fa3b17d179a303b13a1973161bccf8f0874da
Roman <roman.berti09@gmail.com>
2023-03-24 14:37:34 +0100

    Choleski abgeschlossen

--- Praktikum 5/A1.py
+++ Praktikum 5/A1.py
@@ -1,41 +1,47 @@
 import numpy as np
 from numpy.linalg import norm
 
-#Kronecker selber implementieren um Probleme mit Transponieren zu vermeiden
-def Kronecker(w):
-    res = np.zeros([len(w),len(w)],dtype=float)
-    for i in range(len(w)):
-        res[:,i] = w * w[i]
-    return(res)
 
-#Householder transformation implementieren 
+# Kronecker selber implementieren um Probleme mit Transponieren zu vermeiden
+def Kronecker(w):
+    res = np.zeros([len(w), len(w)], dtype=float)
+    for i in range(len(w)):
+        res[:, i] = w * w[i]
+    return (res)
+
+
+# Householder transformation implementieren
 def HouseholderTransformation(w):
-    H = np.eye(len(w)) - (Kronecker(w) / (0.5 * np.dot(w,w)))
+    H = np.eye(len(w)) - (Kronecker(w) / (0.5 * np.dot(w, w)))
     return H
 
-#mysign Funktion definieren da bei 0 auch 1 returned werden muss
-def mysign(x): # numpy sign liefert 0 für 0
+
+# mysign Funktion definieren da bei 0 auch 1 returned werden muss
+def mysign(x):  # numpy sign liefert 0 für 0
     if x >= 0:
         return 1
     else:
         return -1
-    
-#Einheitsvektor generieren
-def e(n):
-    return np.array([1]+[0 for k in range(n-1)])
 
-#Array vorgeben
-A = np.array([[-1,  7, -8, -9,  6],
-       [-6, -8,  0,  3,  8],
-       [-4, -2,  8,  0, -2],
-       [-1, -9,  4, -8,  2],
-       [-3, -5, -5,  7, -4],
-       [-7, -4,  7, -1,  5],
-       [-9, -7,  6, -5, -8],
-       [-4, -3, -5,  3, -6],
-       [ 5,  7,  5, -4, -5],
-       [ 4, -6, -8, -2, -5]],dtype=float)
-m,n = A.shape
+
+# Einheitsvektor generieren
+def e(n):
+    return np.array([1] + [0 for k in range(n - 1)])
+
+
+# Array vorgeben
+imported_data = np.loadtxt("data.txt")
+x,y= imported_data[:,0],imported_data[:,1]
+
+
+#---------------------Ausgleichsrechnung--------------------------------------------
+n = 4
+A = np.array(np.zeros([n,len(x)]))
+A[0,]= 1
+for o in range(1,n):
+    A[o,0:] = np.power(x[:], (4-o))
+
+m, n = A.shape
 
 # Das ganze Array durchlaufen
 Anew = A.copy()
@@ -43,18 +49,17 @@ onesmax = np.eye(m)
 Q = np.eye(m)
 
 for k in range(n):
-    print('Spalte '+str(k+1))
-    y = Anew[k:,k]
+    print('Spalte ' + str(k + 1))
+    y = Anew[k:, k]
     w = y.T + mysign(y[0]) * np.linalg.norm(y) * e(len(y))
     Qk = HouseholderTransformation(w)
-    Q = Q@(onesmax + np.pad(Qk,[(k,0),(k,0)]) - np.pad(np.eye(m-k),[(k,0),(k,0)]))
-    Anew[k:,k:] = Qk@Anew[k:,k:]
+    Q = Q @ (onesmax + np.pad(Qk, [(k, 0), (k, 0)]) - np.pad(np.eye(m - k), [(k, 0), (k, 0)]))
+    Anew[k:, k:] = Qk @ Anew[k:, k:]
 
-#R = Anew[:n,:n]
-#Q = Q[:n,:]
+# R = Anew[:n,:n]
+# Q = Q[:n,:]
 R = Anew
-print(np.round(R,4))
+print(np.round(R, 4))
 print(Q)
 
-print(np.round(Q@R - A,4))
-
+print(np.round(Q @ R - A, 4))
--- Praktikum 5/A2.py
+++ Praktikum 5/A2.py
@@ -7,20 +7,20 @@ from scipy.linalg import solve_triangular
 def lorentz(x, a, x0, sigma):
     return a / (1 + ((x - x0)/(sigma/2))**2)
 
-# Generate some test data
+
+
+
 imported_data = np.loadtxt("data.txt")
 x,y= imported_data[:,0],imported_data[:,1]
-# Estimate the baseline using a polynomial fit
-#baseline = np.polyval(np.polyfit(x, y, deg=3), x) #Hier noch eine lineare ausgleichsrechnung!!!
-corrected = y
+
 
 #---------------------Ausgleichsrechnung--------------------------------------------
 n = 4
-A = np.array(np.zeros([n,len(x)]))
-A[0,]= 0.1
+A = np.array(np.zeros([n+1,len(x)]))
+A[0,]= 1
 for o in range(1,n):
     A[o,0:] = np.power(x[:], (4-o))
-
+A[4,0:]=lorentz(x[:],1,80.3e3,100)
 
 A = A.T
 #-----A^TA ausrechnen---------------------
@@ -36,7 +36,7 @@ los = solve_triangular(L.T ,los,lower = False)
 
 
 fit = np.array(np.zeros(len(x)))
-fit[:] = (A @ los)
+fit[:] = (A[0:,0:4] @ los[0:4])
 baseline = fit
 #-----------------------------------------------------------------------------------
 
@@ -44,15 +44,14 @@ baseline = fit
 residual = y - baseline
 
 # Fit the residual with the Lorentz function
-popt, pcov = curve_fit(lorentz, x, residual, p0=[1, 80.3e3, 100])
-
-# Subtract the Lorentz peak from the residual to obtain the baseline-corrected data
-corrected = residual * lorentz(x, *popt)
-
+#popt, pcov = curve_fit(lorentz, x, residual, p0=[1, 80.3e3, 100])
+#eigener fit:
+#corrected = lorentz(x, *popt)
+corrected = lorentz(x[:],los[4],80.3e3,100)
 # Plot the original data, estimated baseline, and corrected data
 plt.plot(x, y, label='Original Data')
 plt.plot(x, baseline, label='Estimated Baseline')
-plt.plot(x, corrected, label='Baseline-Corrected Data')
+plt.plot(x, corrected, label='estimated Data')
 plt.plot(x, residual, label='Residual')
 plt.legend()
 plt.show()
