8e8a74c707f2c7e5d7c416409936c10cce3c1eaf
Roman <roman.berti09@gmail.com>
2023-03-28 14:06:08 +0200

    Jakobi matrix aufgestellt
    erste spalte nach Phi1 abgeleitet

--- Praktikum 6/Parkt6Berti.py
+++ Praktikum 6/Parkt6Berti.py
@@ -10,6 +10,11 @@ class Robot:
         self.L2 = np.array([L2]) #Länge des armes, welcher an dem ersten Arm befestigt ist und dessen endpunkt der Greifer ist
         self.pos_c = self.cart(self) #aktuelle position des Greifers in Kartesischen koordinaten
         self.pos_p = self.polar(self)#aktuelle position des Greifers in Polar koordinaten
+        self.jako = np.array([[2*self.L1*self.L2*np.cos(np.pi-self.Phi2)*np.sin(self.Phi2+self.Phi1),
+        -2*self.L1*self.L2*np.sin(np.pi-self.Phi2)*np.cos(self.Phi2+self.Phi1)+2*self.L1*self.L2*np.cos(np.pi-self.Phi2)*np.sin(self.Phi2+self.Phi1)],
+        [-2*self.L1*self.L2*np.cos(np.pi-self.Phi2)*np.sin(self.Phi2+self.Phi1),
+                -2*self.L1*self.L2*np.sin(np.pi-self.Phi2)*np.sin(self.Phi2+self.Phi1)-2*self.L1*self.L2*np.cos(np.pi-self.Phi2)*np.cos(self.Phi2+self.Phi1)]])
+
     def cart(self): #funktion zur bestimmung der Kartesischen koordinaten des greifers aus den 2 winkeln und den Längen der Arme
         v = np.array([self.L1**2+self.L2**2-2*self.L1*self.L2*np.cos(np.pi-self.Phi2)*np.cos(self.Phi1+self.Phi2),
                       self.L1**2+self.L2**2-2*self.L1*self.L2*np.cos(np.pi-self.Phi2)*np.sin(self.Phi1+self.Phi2)])
